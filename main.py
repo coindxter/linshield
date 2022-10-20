48748f2126e1f9acc34e095c576b2b7d421e1be7
jared <jaredcrace@gmail.com>
2022-10-19 21:52:34 -0500

    appending to file

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import argparse
 import subprocess
 
 
+def append_to_file(str, file_name):
+    print('appending: %s' % str)
+    print('to file: %s' % file_name)
+
+    ans = input('Do you want to continue? [Y/n]: ')
+    if ans not in ('Y', 'y'):
+        print('canceling the command')
+        return
+
+    # open file in append mode
+    f = open(file_name, "a")
+    f.write(str)
+    f.close()
 
 def command(arguments):
     print(arguments)
@@ -34,11 +47,11 @@ if __name__ == '__main__':
                         help='verbose output',
                         default=None)
 
-    command(['ls', '-l'])
-    command(['apt-get','install','aide', 'aide-common'])
-    command(['aideinit'])
-    command(['mv','/var/lib/aide/aide.db.new','/var/lib/aide/aide.db'])
-    command(['chown','root:root','/etc/systemd/system/aidecheck.*' ])
+    #command(['ls', '-l'])
+    #command(['apt-get','install','aide', 'aide-common'])
+    #command(['aideinit'])
+    #command(['mv','/var/lib/aide/aide.db.new','/var/lib/aide/aide.db'])
+    #command(['chown','root:root','/etc/systemd/system/aidecheck.*' ])
 
-    
+    append_to_file('fs.suid_dumpable = 0\n', 'test_file.txt')
 

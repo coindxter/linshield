e4d20bc01075803a03531c1de62f82c85a185322
jared <jaredcrace@gmail.com>
2022-10-22 13:37:10 -0500

    added insert in file function

--- src/main.py
+++ src/main.py
@@ -2,6 +2,20 @@ import argparse
 import subprocess
 import os
 
+def insert_in_file(insert_str, file_name, search_str):
+    with open(file_name, 'r+') as fd:
+        contents = fd.readlines()
+
+        if search_str in contents[-1]:  # Handle last line to prevent IndexError
+            contents.append(insert_str)
+        else:
+            for index, line in enumerate(contents):
+                if search_str in line:
+                    contents.insert(index + 1, insert_str)
+                    break
+        fd.seek(0)
+        fd.writelines(contents)
+
 
 def append_to_file(str, file_name):
     print('appending to file: %s' % file_name)
@@ -54,6 +68,7 @@ if __name__ == '__main__':
                         default=None)
 
 
+    '''
     #Initialize AIDE
     command(['aideinit'])
     command(['mv','/var/lib/aide/aide.db.new','/var/lib/aide/aide.db'])
@@ -68,9 +83,12 @@ if __name__ == '__main__':
     command(['systemctl','is-active','apport.service'])
     #Ensure permissions on /etc/motd are configured
     command(['chown', 'root:root', '$(readlink -e /etc/motd)'])
+    '''
 
 
     
+#    insert_in_file('*****\n', r"/Users/jaredcrace/data/tasks/owen_prj/owen_new_prj/Ubuntu_Script/src/test_file.txt", 'three')
+#    insert_in_file('dog\n', 'test_file.txt', 'test_marker')
 
     #append_to_file('fs.suid_dumpable = 0\n', 'test_file.txt')
 

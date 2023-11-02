950b9908993831add0fd6afc23dc215fe06d7762
owen <owencrace@gmail.com>
2023-11-01 21:12:29 -0500

    sdff

--- CPL-2023-main /MODS/0 - Set Up/mod.sh
+++ CPL-2023-main /MODS/0 - Set Up/mod.sh
@@ -8,4 +8,4 @@ pinfo "Timestamp: `date --rfc-3339=seconds`"
 
 todo "Launch two root shells in another terminal in case something goes wrong"
 todo "Change default user password to 'password' (do in user settings in most cases)"
-todo "Replace default user's .bash_history with an actual file"
+todo "Replace default user's .bash_history with an actuall file"

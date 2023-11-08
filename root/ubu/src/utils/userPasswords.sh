83eb9031b4d6b68f3c8d418256e56930b45887df
owen <owencrace@gmail.com>
2023-11-07 22:19:15 -0600

    uupdated readme

--- root/ubu/src/utils/userPasswords.sh
+++ root/ubu/src/utils/userPasswords.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define auto-login user
+read -p "Please enter the auto-login user (the user you want to exclude from changes): " autoLoginUser
+
 # Define authorized users and admins
 read -p "Please enter the authorized users (comma-separated, no spaces): " authorizedUsers
 read -p "Please enter the authorized admins (comma-separated, no spaces): " authorizedAdmins
@@ -20,13 +23,13 @@ unauthorizedAdmins=()
 missingAdmins=()
 
 for user in "${currentUsers[@]}"; do
-    if [[ ! " ${authorizedUsers[@]} " =~ " $user " ]] && ! [[ " ${defaultAccounts[@]} " =~ " $user " ]]; then
+    if [[ ! " ${authorizedUsers[@]} " =~ " $user " ]] && ! [[ " ${defaultAccounts[@]} " =~ " $user " ]] && [ "$user" != "$autoLoginUser" ]; then
         unauthorizedUsers+=("$user")
     fi
 done
 
 for user in "${authorizedUsers[@]}"; do
-    if [[ ! " ${currentUsers[@]} " =~ " $user " ]]; then
+    if [[ ! " ${currentUsers[@]} " =~ " $user " ]] && [ "$user" != "$autoLoginUser" ]; then
         missingUsers+=("$user")
     fi
 done
@@ -53,14 +56,9 @@ echo "Missing admins: ${missingAdmins[*]}"
 read -p "Would you like to fix these issues? (y/n): " consent
 
 if [ "$consent" == "y" ]; then
-    # Remove unauthorized users
+    # Remove unauthorized users from sudo group
     for user in "${unauthorizedUsers[@]}"; do
-        sudo userdel -r "$user"
-    done
-
-    # Remove unauthorized admins from sudo group
-    for admin in "${unauthorizedAdmins[@]}"; do
-        sudo deluser "$admin" sudo
+        sudo deluser "$user" 
     done
 
     # Add missing users
@@ -72,29 +70,33 @@ if [ "$consent" == "y" ]; then
     for admin in "${missingAdmins[@]}"; do
         sudo usermod -aG sudo "$admin"
     done
+    
+    # Remove unauthorized admins from sudo group
+    for admin in "${unauthorizedAdmins[@]}"; do
+        sudo deluser "$admin" sudo
+        echo "Removed admin: $admin from sudo group"
+    done
+
 
     # Change passwords for all users
     newPassword="Student123!"
     for user in "${currentUsers[@]}"; do
-        echo -e "$newPassword\n$newPassword" | sudo passwd "$user"
-        echo "Password changed for: $user"
+        if [ "$user" != "$autoLoginUser" ]; then
+            echo -e "$newPassword\n$newPassword" | sudo passwd "$user"
+            echo "Password changed for: $user"
+        fi
     done
 
     # Disable password aging for all users
     for user in "${currentUsers[@]}"; do
-        sudo chage -I 1 -m 1 -M 30 -E 7 "$user"
-        echo "Password aging disabled for: $user"
+        if [ "$user" != "$autoLoginUser" ]; then
+            sudo chage -I -1 -m 0 -M 99999 -E -1 "$user"
+            echo "Password aging disabled for: $user"
+        fi
     done
 
-    echo "Passwords updated and password aging disabled for all users."
-
-    echo "$newPassword" | sudo tee /etc/secure_password.txt > /dev/null
-
-    echo "New password saved to /etc/secure_password.txt"
+    echo "Changes applied."
 
 else
-
     echo "Nothing changed"
-
 fi
-

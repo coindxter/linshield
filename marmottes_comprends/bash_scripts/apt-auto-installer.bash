af8c1eb3b72668289fc3314b859ebafba873f090
owen <owencrace@gmail.com>
2024-11-02 19:13:25 -0500

    fixed command errors

--- marmottes_comprends/bash_scripts/apt-auto-installer.bash
+++ marmottes_comprends/bash_scripts/apt-auto-installer.bash
@@ -1,10 +1,15 @@
-# Determine the OS number and version
+#!/bin/bash
+
+# Determine OS number and version
 OS_NAME=$(lsb_release -si)
 OS_VERSION=$(lsb_release -sr)
 
-# Update the package list
-if [ "$OS_NAME" == "Ubuntu" ]; then
-    if [ "$(sh cat "../support-files/apt_sources/$OS_VERSION")" != "" ]; then
+# Create backups directory if not present
+[ ! -d "../backups" ] && mkdir -p "../backups"
+
+# Update the package list if Ubuntu and supported version
+if [[ "$OS_NAME" == "Ubuntu" ]]; then
+    if [[ -s "../support-files/apt_sources/$OS_VERSION" ]]; then
         sudo cp /etc/apt/sources.list ../backups/sources.list.$(date +%Y-%m-%d)
         sudo cat "../support-files/apt_sources/$OS_VERSION" > /etc/apt/sources.list
     else
@@ -14,12 +19,12 @@ else
     echo "OS $OS_NAME not supported"
 fi
 
-# list the files in the /etc/apt/sources.list.d directory if any exist ask the user if they want to remove then if so backup the files and remove them
-if [ "$(ls /etc/apt/sources.list.d)" != "" ]; then
+# List and potentially remove files in /etc/apt/sources.list.d
+if [[ -n "$(ls /etc/apt/sources.list.d 2>/dev/null)" ]]; then
     echo "The following files exist in /etc/apt/sources.list.d"
     for file in /etc/apt/sources.list.d/*; do
         echo -n "Do you want to remove $file? (y/n) " && read REMOVE_FILE
-        if [ "$REMOVE_FILE" == "y" ]; then
+        if [[ "$REMOVE_FILE" == "y" ]]; then
             sudo cp -r "$file" "../backups/sources.list.d.$(date +%Y-%m-%d)"
             sudo rm -r "$file"
         else
@@ -31,14 +36,8 @@ fi
 # Update the package list
 sudo apt-get update
 
-# Update APT
+# Upgrade APT package itself
 sudo apt-get install --only-upgrade apt
 
-# install app armor
-sudo apt-get install apparmor apparmor-utils apparmor-profiles -y
-# install fail2ban
-sudo apt-get install fail2ban -y
-# install libpam-pwquality
-sudo apt-get install libpam-pwquality -y
-# unattended-upgrades
-sudo apt-get install unattended-upgrades -y
+# Install security tools
+sudo apt-get install -y apparmor apparmor-utils apparmor-profiles fail2ban libpam-pwquality unattended-upgrades

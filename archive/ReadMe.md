980dbb44064d7191d89aeb8c69e929c63197e248
owen <owencrace@gmail.com>
2025-08-26 13:30:16 -0500

    cleaning up

--- archive/ReadMe.md
+++
@@ -1,1 +0,0 @@
- This is the archive of old/new scripts and information such as readme's and past scoring reports
--- run/main.py
+++
@@ -1,3 +0,0 @@
-#main script to harden ubuntuu 
-print('This is the main script to run everything. Below are options of things to run in the recomended order')
-print("test")
